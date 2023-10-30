9c5fd1c704288af1cfa1a155188fa7d11fce0d86
unknown <syahsahrulalam58@gmail.com>
2023-10-30 15:20:38 +0700

    alam

---
+++ database/migrations/2023_10_23_061920_create_students_table.php
@@ -0,0 +1,20 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->char('nim', 8);
+            $table->string('nama', 30);
+            $table->string('prodi', 30);
+            $table->timestamps();
+        });
+    }
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
